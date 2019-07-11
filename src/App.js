d9968ccaf06e82903c7f09e68d89159f41988aa0
Student <jsitbon@qcc.cuny.edu>
2019-07-11 17:54:33 -0400

    'all

--- src/App.js
+++ src/App.js
@@ -11,47 +11,50 @@ class App extends Component {
     this.state ={
       pokemons: [],
       pokeAbilities: [],
-      pokeImage: [],
+      pokeImages: [],
       // temporary: []
     }
   }
 
-  getImage =() =>{
-    this.state.pokemons.map(pokemon =>{
-      this.fetchImage(pokemon)
+     getImage(pokemons){
+    return pokemons.map(pokemon =>{
+      return this.fetchImage(pokemon)
     })
   }
 
-  async fetchImage(pokemon){
-    console.log('[App.js] getImage()');
+    async fetchImage(pokemon){
+    // console.log('[App.js] fetchImage()');
+    // console.log('pokemon name', pokemon.name)
     try {
-    const image = await fetch(`https://pokeapi.co/api/v2/${pokemon.name}`)
-    const imgJson = image.json();
-
-    this.setState({
-        pokeImage: imgJson
-    })
+    const image = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+    const imgJson = await image.json();
+    // console.log('fetchImage in try', imgJson)
+    return await imgJson;
       
     } catch (error) {
-      
+      console.log('fetchImage', error)
     }
   }
 
   async componentDidMount(){
-    console.log('[App.js] componentDidMount')
+    // console.log('[App.js] componentDidMount')
     try {
       const pokeResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=100`);
       const pokeAbilities = await fetch(`https://pokeapi.co/api/v2/ability/?limit=100`);
       // const eachPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto/`)
-      // const eachPk = eachPokemon.json();
-      // console.log('eachPk', eachPk.sprites)
+      // const eachPk = await eachPokemon.json();
+      // console.log(typeof eachPk)
+      // console.log('eachPk', eachPk.sprites.back_default)
       const pokeJson = await pokeResponse.json();
-      const  abilityJson = await pokeAbilities.json();
+      const abilityJson = await pokeAbilities.json();
+      const pokeImage = await this.getImage(pokeJson.results);
+      console.log('componentDidMount pokeImage: ', pokeImage);
       console.log('componentDidMount pokeJson.results', pokeJson.results);
-      console.log('abilityJson', abilityJson.results);
+      // console.log('abilityJson', abilityJson.results);
       this.setState({
         pokemons: pokeJson.results,
         pokeAbilities: abilityJson.results,
+        pokeImages: pokeImage
         // temporary: eachPk
       })
     } catch (error) {
@@ -59,22 +62,29 @@ class App extends Component {
     }
   }
 
-  getPokemonInfo = () =>{
-    
-  return this.state.pokemons;
-
-  }
 
   render(){
     return (
       
       this.state.pokemons.length > 0 ? 
       (<Playground>
-        {console.log('images: ', this.state.temporary)}
-        <Pokemons name={this.state.pokemons} ability={this.state.pokeAbilities} id='0'/>
-        <Pokemons name={this.state.pokemons} ability={this.state.pokeAbilities} id='1'/>
-        <Pokemons name={this.state.pokemons} ability={this.state.pokeAbilities} id='2'/>
-        <Pokemons name={this.state.pokemons} ability={this.state.pokeAbilities} id='3'/>
+        {console.log('images: ', this.state.pokeImage)}
+        <Pokemons 
+        name={this.state.pokemons} 
+        ability={this.state.pokeAbilities} 
+        id='0' image={this.state.pokeImage}/>
+        <Pokemons 
+        name={this.state.pokemons} 
+        ability={this.state.pokeAbilities} 
+        id='1'/>
+        <Pokemons 
+        name={this.state.pokemons} 
+        ability={this.state.pokeAbilities} 
+        id='2'/>
+        <Pokemons 
+        name={this.state.pokemons} 
+        ability={this.state.pokeAbilities} 
+        id='3'/>
         
       </Playground>) : (null)
     );
