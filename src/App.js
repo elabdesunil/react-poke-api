f5cf198a15cbaee813ab397eaf6a93ae15116f62
sunilale0 <sunilale0@gmail.com>
2019-07-14 03:06:49 -0400

    successfully reached the images inside pokeInfo

--- src/App.js
+++ src/App.js
@@ -11,68 +11,105 @@ class App extends Component {
     this.state ={
       pokemons: [],
       pokeAbilities: [],
-      pokeImages: [],
-      // temporary: []
+      pokeInfo: []
     }
   }
 
-     getImage(pokemons){
-    return pokemons.map(pokemon =>{
-      return this.fetchImage(pokemon)
-    })
-  }
+  // async somefunct(pokeAddress) {
+  //   let someVar = await fetch(`${pokeAddress}`);
+  //   let varJsoned = await someVar.json();
+  //   console.log('someVar in somefunct jsoned', varJsoned)
+  //   return varJsoned;
+  // } 
 
-    async fetchImage(pokemon){
-    // console.log('[App.js] fetchImage()');
-    // console.log('pokemon name', pokemon.name)
-    try {
-    const image = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-    const imgJson = await image.json();
-    // console.log('fetchImage in try', imgJson)
-    return await imgJson;
-      
-    } catch (error) {
-      console.log('fetchImage', error)
-    }
-  }
 
+  
   async componentDidMount(){
-    // console.log('[App.js] componentDidMount')
     try {
       const pokeResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=100`);
       const pokeAbilities = await fetch(`https://pokeapi.co/api/v2/ability/?limit=100`);
-      // const eachPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto/`)
-      // const eachPk = await eachPokemon.json();
-      // console.log(typeof eachPk)
-      // console.log('eachPk', eachPk.sprites.back_default)
+      console.log('pokeResponse without json conversion', pokeResponse)
       const pokeJson = await pokeResponse.json();
       const abilityJson = await pokeAbilities.json();
-      const pokeImage = await this.getImage(pokeJson.results);
-      console.log('componentDidMount pokeImage: ', pokeImage);
+
+      const pkNames = pokeJson.results.map(pokemon => {
+        return pokemon.name
+      });
+      console.log('pkNames: ', pkNames)
+
+      let pkInfo = [];
+      for (let i = 0; i < pkNames.length; i++) {
+        let tempPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkNames[i]}`);
+        let tempPokeJsoned = await tempPoke.json();
+        pkInfo.push(tempPokeJsoned);
+      }
+      console.log('pkInfo ', pkInfo);
       console.log('componentDidMount pokeJson.results', pokeJson.results);
-      // console.log('abilityJson', abilityJson.results);
       this.setState({
         pokemons: pokeJson.results,
         pokeAbilities: abilityJson.results,
-        pokeImages: pokeImage
-        // temporary: eachPk
+        pokeInfo: pkInfo
       })
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
   }
 
+  imageArray = () => {
+    let arr = [];
+    for (let i = 0; i < 5; i++)
+      arr.push(this.state.pokeInfo[i].sprites.back_default)
+    return arr;
+  }
 
-  render(){
+
+
+  giveImage = () => {
+    console.log('console logging this.state.pokeInfo[0]["sprites"]back_default', (this.state.pokeInfo[0].sprites.back_default))
+    return this.state.pokeInfo[0].sprites.back_default;
+      return 'text';
+    }
+  
+  giveDetails = () => {
+    var rows = [];
+    for (var i = 0; i < 5; i++) {
+      rows.push(<img src='this.state.pokeInfo[i].sprites.back_default' alt={i + i + i} size='5%' key={'00'+i}/>);
+    }
+    console.log(rows)
+    return <div>{rows}</div>;
+  }
+    
+
+
+  render() {
+
+    console.log('type of this.state.pokeInfo: '+ typeof this.state.pokeInfo)
+    console.log('this.state.pokeInfo: ', this.state.pokeInfo)
+
+    // let imageArr = this.imageArr;
+    // let someVar = '<img src={ this.state.pokeInfo[0].sprites.back_default }'
+    
     return (
       
-      this.state.pokemons.length > 0 ? 
-      (<Playground>
-        {console.log('images: ', this.state.pokeImage)}
+      this.state.pokemons.length > 0 && this.state.pokeInfo.length > 0 ?
+
+        (<Playground>
+
+          <div>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png'
+              style={{ width: '100%' }}/>
+            <img 
+              src={this.giveImage()}
+              alt="eachpokemon"
+              style={{ width: '100%' }} />
+            
+          </div>
+        {console.log('pokeInfo from state: ', this.state.pokeInfo)}
         <Pokemons 
         name={this.state.pokemons} 
         ability={this.state.pokeAbilities} 
-        id='0' image={this.state.pokeImage}/>
+        id='0' image={this.state.pokeImages}/>
         <Pokemons 
         name={this.state.pokemons} 
         ability={this.state.pokeAbilities} 
@@ -84,7 +121,12 @@ class App extends Component {
         <Pokemons 
         name={this.state.pokemons} 
         ability={this.state.pokeAbilities} 
-        id='3'/>
+            id='3' />
+          {this.giveDetails()}
+          
+        
+          
+        
         
       </Playground>) : (null)
     );
